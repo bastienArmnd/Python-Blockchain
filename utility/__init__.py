1b1063e1afe9fe5e58e842ea69733e3b02c128d7
BatStudio <45672431+Batino@users.noreply.github.com>
2019-06-02 11:05:51 +0800

    full commit

---
+++ utility/__init__.py
@@ -0,0 +1,3 @@
+##from utility._hash_file import hash_string_256
+##
+##__all__ = ['hash_string_256']
---
+++ utility/_hash_file.py
@@ -0,0 +1,16 @@
+import hashlib as hl
+import json
+
+##__all__ = ['hash_string256','hash_block']
+           
+def hash_string256(string):
+    return hl.sha256(string).hexdigest()
+
+def hash_block(block):
+    """ Hashes a block and returns a string representation of it"""
+
+    hashable_block = block.__dict__.copy()
+    hashable_block['transactions'] = [tx.to_ordered_dict() for tx in hashable_block['transactions']]
+    return hash_string256(json.dumps(hashable_block, sort_keys = True).encode())
+
+# -------------------------------------------------------------------
